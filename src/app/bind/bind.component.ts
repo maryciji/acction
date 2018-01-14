1f8a13c513fab0e55edbb2e18c8bb80112325a34
269826537@qq.com <269826537@qq.com>
2018-01-14 08:34:00 +0800

    1.0.2

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -10,9 +10,9 @@ export class BindComponent implements OnInit {
 
   constructor() {
     Observable.from([1, 2, 3, 4])
-      .filter(e => e % 2 === 0)
-      .map(e => e * e)
-      .subscribe(
+      .filter(e => e % 2 === 0) /*过滤*/
+      .map(e => e * e) /*映射*/
+      .subscribe( /*订阅*/
         e => console.log(e),
         err => console.error(err),
         () => console.log('结束啦')
